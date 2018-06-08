4f7e53017f23adfb1080da9d116af3d4ef847b8b
Eric Pratt <ericpratt86@gmail.com>
2018-06-07 18:06:24 -0700

    add messages form

--- public/js/index.js
+++ public/js/index.js
@@ -2,4 +2,21 @@ const socket = io();
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+
+  const li = document.createElement('li');
+  li.innerHTML = `${message.from}: ${message.text}`;
+
+  document.getElementById('messages').appendChild(li);
+
+});
+
+const messageForm = document.getElementById('message-form');
+
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'user',
+    text: document.getElementsByName('message')[0].value
+  });
 });
